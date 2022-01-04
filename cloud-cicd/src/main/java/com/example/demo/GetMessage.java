1dfd2c8f2e4f0f74913ed96ff995576958a32d11
002VI0744 <002VI0744@LAPTOP-UJ2HG60P>
2022-01-04 17:59:13 +0530

    commit first

--- cloud-cicd/src/main/java/com/example/demo/GetMessage.java
+++ cloud-cicd/src/main/java/com/example/demo/GetMessage.java
@@ -2,7 +2,9 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class GetMessage {
 
 	
